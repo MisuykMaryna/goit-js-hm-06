6e81d73d0ab2b2696f7912c21c11c5e06565f24d
MisuykMaryna <misyuk.marina@gmail.com>
2023-02-07 20:58:29 +0100

    hm

--- src/src/js/task-10.js
+++ src/src/js/task-10.js
@@ -1,32 +1,32 @@
-const refs = {
-  inputEl: document.querySelector("input"),
-  createBtn:document.querySelector("[data-create]"),
-  destroyBtn:document.querySelector("[data-destroy]"),
-  boxesEl:document.querySelector("#boxes"),
-}
-
-
-const makeBoxes = (amount) => {
-  amount = Number(refs.inputEl.value);
-  const arr = new Array(Number(amount)).fill("");
-  const markup = arr.map((value, index) => {
-    value + `<div style=
-    width: ${30 + index * 10} px
-      height: ${30 + index * 10} px
-      backgroundcolor: ${getRandomHexColor()}>
-    </div>`
-  }).join("");
- refs.boxesEl.insertAdjacentHTML("beforeend", markup)
-
-}
-  function getRandomHexColor() {
+function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-const removeBoxes = () => {
-  refs.boxesEl.innerHTML = "";
-  refs.inputEl.value = "";
-}
+// const inputEl = document.querySelector("[type ='number']");
+// const createBtn = document.querySelector("[data-create]");
+// const destroyBtn = document.querySelector("[data-destroy]");
+// const boxesEl = document.querySelector("#boxes");
 
-refs.createBtn.addEventListener("click", makeBoxes);
-refs.destroyBtn.addEventListener("click", removeBoxes);
+
+
+// const createBoxes = (amount) => {
+//   amount = Number(inputEl.value);
+//   const makeBoxes = new Array(Number(amount)).fill("");
+//    const markup = makeBoxes.map((value, index) => {
+//       value + `<div style=
+//     width: ${30 + index * 10} px
+//       height: ${30 + index * 10} px
+//       background-color: ${getRandomHexColor()}>
+//     </div>`
+//   }).join("");
+//  return boxesEl.insertAdjacentHTML("beforeend", markup);
+
+// }
+
+// const destroyBoxes = () => {
+//   boxesEl.innerHTML = "";
+//   inputEl.value = "";
+// }
+
+// createBtn.addEventListener("click", createBoxes);
+// destroyBtn.addEventListener("click", destroyBoxes);
